a03c35f100f88e82a3addfde74184711cd1faa04
Brian Victor <brian_saka@hotmail.com>
2022-09-25 00:31:34 -0300

    added new ticket page 1/2

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -3,11 +3,11 @@ import Header from '../../components/header/header';
 import Title from '../../components/title/title'
 // import { AuthContext } from '../../context/user'
 import './dashboard.style.css' 
-import { FiMessageSquare, FiPlus } from 'react-icons/fi'
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi'
 import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
-  const [tickets, steTickets] = useState([]);
+  const [tickets, steTickets] = useState([1]);
 
   return (
     <div>
@@ -33,6 +33,39 @@ export default function Dashboard() {
               <FiPlus size={25} color="fff" />
               Novo chamado
             </Link>
+
+            <table>
+
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Criado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Cliente teste</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span className="badge" style={{backgroundColor: "#52b69a"}}>Em aberto</span>
+                  </td>
+                  <td data-label="Criado">24/09/2022</td>
+                  <td data-label="#">
+                    <button className="action" style={{backgroundColor: '#00a896'}}>
+                      <FiSearch color="#f8f9fa" size={17} />
+                    </button>
+                    <button className="action" style={{backgroundColor: '#f3722c'}}>
+                      <FiEdit2 color="#f8f9fa" size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+
+            </table>
           </>
         )}
 
